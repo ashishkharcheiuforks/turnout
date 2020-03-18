0e1e79b9f14a50bfcc994cf70fb0de8e9802816d
Josh Levinger <josh@levinger.net>
2020-03-18 14:18:20 -0700

    linting

--- app/register/tasks.py
+++ app/register/tasks.py
@@ -1,6 +1,6 @@
+import logging
 import shutil
 import tempfile
-import logging
 
 from celery import shared_task
 
@@ -9,8 +9,8 @@ from common.pdf import fill_form
 
 from .models import Registration
 
-
 logger = logging.getLogger("register")
+
 
 @shared_task
 @statsd.timed("turnout.register.process_registration_submission")
@@ -18,21 +18,23 @@ def process_registration_submission(registration_pk, state_id):
     registration = Registration.objects.get(pk=registration_pk)
 
     # TODO, select template based on state
-    template_path = 'register/templates/pdf/eac-nvra.pdf'
+    template_path = "register/templates/pdf/eac-nvra.pdf"
 
     # open file objects
-    template_pdf = open(template_path,'rb')
-    filled_pdf = tempfile.NamedTemporaryFile()
+    template_pdf = open(template_path, "rb")
+    filled_pdf = tempfile.NamedTemporaryFile(mode="wb")
 
     # convert Registration to dict
     registration_data = registration.__dict__
+    # some fields need to be converted to string representation
+    # state, date_of_birth, enums
 
     # fill from dict
     fill_form(template_pdf, filled_pdf, registration_data)
 
     # TODO upload to s3
     # TEMP copy to /app/ to be visible
-    shutil.copyfile(filled_pdf.name, '/app/register-tmp.pdf')
+    shutil.copyfile(filled_pdf.name, "/app/register-tmp.pdf")
 
     # remove temporary file
     filled_pdf.close()
--- app/register/validators.py
+++ app/register/validators.py
@@ -1,7 +1,5 @@
 import logging
 
-from rest_framework import serializers
-
 logger = logging.getLogger("register")
 
 
