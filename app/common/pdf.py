ad465ce17262385313489e55e92da24da1d2be2d
Josh Levinger <josh@levinger.net>
2020-03-18 15:47:22 -0700

    pdf fill_form handle multiple pages

--- app/common/pdf.py
+++ app/common/pdf.py
@@ -15,25 +15,26 @@ def fill_form(input_file, output_file, data):
     data is dictionary with keys corresponding to the form fields"""
 
     the_pdf = pdfrw.PdfReader(input_file)
-    annotations = the_pdf.pages[0][ANNOT_KEY]
-    for annotation in annotations:
-        if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_kEY:
-            key = annotation[ANNOT_FIELD_KEY][1:-1]
-            if key in data.keys():
-                val = data[key]
-                if val == None:
-                    # skip nulls
-                    continue
-                if val == True:
-                    # treat booleans as checkboxes
-                    annotation.update(pdfrw.PdfDict(V=pdfrw.PdfName("On")))
-                else:
-                    # set annotation value
-                    annotation.update(pdfrw.PdfDict(V="{}".format(val)))
-                    # and empty appearance to make field visible in Apple Preview
-                    annotation.update(pdfrw.PdfDict(AP=""))
-                # mark the fields as un-editable
-                annotation.update(pdfrw.PdfDict(Ff=1))
+    for page in the_pdf.pages:
+        annotations = page[ANNOT_KEY]
+        for annotation in annotations:
+            if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_kEY:
+                key = annotation[ANNOT_FIELD_KEY][1:-1]
+                if key in data.keys():
+                    val = data[key]
+                    if val == None:
+                        # skip nulls
+                        continue
+                    if val == True:
+                        # treat booleans as checkboxes
+                        annotation.update(pdfrw.PdfDict(V=pdfrw.PdfName("On")))
+                    else:
+                        # set annotation value
+                        annotation.update(pdfrw.PdfDict(V="{}".format(val)))
+                        # and empty appearance to make field visible in Apple Preview
+                        annotation.update(pdfrw.PdfDict(AP=""))
+                    # mark the fields as un-editable
+                    annotation.update(pdfrw.PdfDict(Ff=1))
 
     # set NeedAppearances to ensure the fields are visible in Adobe Reader
     the_pdf.Root.AcroForm.update(pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject("true")))
